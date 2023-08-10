639f320c9dfe4b6299191c746d283d7d00a674c5
theaashiq <mdaashiqin2000@gmail.com>
2023-08-10 10:43:42 +0530

    Hotel Menu Card

---
+++ src/components/basics/Resturant.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import "./style.css";
+import Menu from "./menuApi"
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [
+    ... new Set(Menu.map((curElem) => {
+    return curElem.category;
+    }
+   )
+  ), "All",
+];
+
+console.log(uniqueList);
+
+
+const Resturant = () => {
+   
+    const [menuData, setMenuData] = useState(Menu); 
+    const [menuList, setMenuList] = useState(uniqueList);
+
+    const filterItem = (category) => {
+
+        if(category === "All") {
+            setMenuData(Menu);
+            return;
+         }
+
+        const updateList = Menu.filter((curElem) => {
+            return curElem.category === category;
+        });
+
+        setMenuData(updateList);
+    };
+  return (
+    <>
+      
+      <Navbar filterItem={filterItem} menuList = {menuList}/>
+      <MenuCard menuData={menuData}/>
+    </>
+  );
+};
+
+export default Resturant
---
+++ src/components/basics/menuApi.js
@@ -0,0 +1,187 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/idli.webp",
+      name: "Idli with vada sambar",
+      category: "breakfast",
+      price: "25₹",
+      description:
+        "Soft idlis, crispy vada, and aromatic sambar. A quintessential South Indian delight Enjoy 3 idlis and 1 vada per plate Packed with flavors and served hot Price: Rs 25 per plate",
+    },
+  
+    {
+      id: 3,
+      image: "images/dosa.jpg",
+      name: "Dosa",
+      category: "breakfast",
+      price: "30₹",
+      description:
+        "Thin, crispy, and bursting with flavor. A South Indian delight loved by all, Enjoy 2 dosas per plate. Served with chutney and sambar, Price: Rs 20 per plate ",    },
+    {
+      id: 4,
+      image: "../images/briyani.jpg",
+      name: "Chicken Briyani",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Experience the delicious blend of fragrant rice, tender meat, and aromatic spices in our Biryani. Slow-cooked to perfection, it's a delightful dish full of tradition and flavor. Treat yourself to our special Biryani for a truly satisfying meal, Price: Rs 150 per plate",
+    },
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        " Crispy goodness of our Allu Pakoda. Sliced potatoes coated in a flavorful batter and fried to perfection. Elevate your dining experience with this classic snack that's both crunchy and satisfying. Price Rs 20 per plate",
+    },
+    
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Experience delight with every slice of our scrumptious pizza. Freshly baked crust topped with melted cheese and your favorite ingredients. Elevate your dining experience with our mouthwatering pizza, a perfect blend of flavors in every bite. Price Rs 80 per plate ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "120₹",
+      description:
+        "Delight in our Non-Veg Thali, a complete meal that satisfies your cravings. Savory and succulent meat dishes paired with fragrant rice, assorted sides, and fresh bread. Elevate your dining experience with this wholesome and flavorful feast, offering a taste of variety in every plate. Price Rs 120 per plate ",
+    },
+    {
+      id: 7,
+      image: "../images/Kesari.jpeg",
+      name: "kesari bath",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Experience the sweetness of our Kesari Bath, a warm and comforting dessert. Made with semolina, ghee, and a touch of saffron, it's a treat for your taste buds. Elevate your dining experience with this traditional delight, a perfect ending to your meal. Price Rs 50 per plate ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Enjoy the crispy and savory delight of our Samosa. A golden pastry filled with a tasty mixture of spices and vegetables. Elevate your dining experience with this popular snack, bursting with flavor in every bite. Price Rs 20 (one) ",
+    },
+    {
+        id: 17,
+        image: "../images/samosa.jpg",
+        name: "Kachri",
+        category: "evening",
+        price: "10₹",
+        description:
+          "Discover the crispy delight of our Kachri, a savory snack with a satisfying crunch. Made from lentil or flour dough, it's a flavorful treat. Elevate your dining experience with this tasty snack, a perfect choice to munch on anytime. Price Rs 10",
+      },
+      {
+        id: 10,
+        image: "../images/masaladosa.jpg",
+        name: "Masala dosa",
+        category: "breakfast",
+        price: "40₹",
+        description:
+          "A culinary masterpiece from South India Crispy golden dosa filled with flavorful potato masala Experience the perfect balance of textures and spices Served with coconut chutney and sambar Price: Rs 40 per plate (1 dosa)",
+      },
+      {
+        id: 11,
+        image: "../images/pongal.jpg",
+        name: "Pongal",
+        category: "breakfast",
+        price: "30₹",
+        description:
+          "A hearty and comforting South Indian delicacy Made with a unique blend of rice, lentils, and spices Served piping hot with a generous dollop of ghee Topped with crunchy cashews and aromatic curry Price: Rs 30 per plate",
+      },
+      {
+        id: 12,
+        image: "../images/puri.jpg",
+        name: "Puri bhaji",
+        category: "breakfast",
+        price: "30₹",
+        description:
+          "Indulge in the flavors of a classic Indian breakfast 2 fluffy, deep-fried puris served with spicy potato bhaji Experience the perfect blend of textures and aromatic spices Accompanied by tangy pickle and refreshing mint chutney Price: Rs 30 per plate (2 puri)",
+      },
+      {
+        id: 13,
+        image: "../images/mbriyani.webp",
+        name: "Mutton Briyani",
+        category: "lunch",
+        price: "200₹",
+        description:
+          "Indulge in the rich flavors of our Mutton Biryani, where tender mutton and aromatic spices meet fragrant rice. Cooked with care, each bite tells a tale of taste and tradition. Elevate your dining experience with our hearty Mutton Biryani, a delicious dish that satisfies every palate. Price: Rs 200 per plate",
+      },
+      {
+        id: 14,
+        image: "../images/meals.jpeg",
+        name: "Meals",
+        category: "lunch",
+        price: "80₹",
+        description:
+          "Discover complete satisfaction with our hearty Meals, thoughtfully prepared for your enjoyment. Delight in a balanced combination of flavors, featuring a main course, sides, and a touch of culinary love. Elevate your dining experience with our wholesome Meals, a delicious and fulfilling choice for any time of day. Price: Rs 80 per plate",
+      },
+      {
+        id: 15,
+        image: "../images/kabab.jpeg",
+        name: "Kabab (Starter)",
+        category: "lunch",
+        price: "100₹",
+        description:
+          "Begin your culinary journey with our delectable Kababs - tender and flavorful starters that awaken your taste buds. Each bite is a harmony of well-marinated ingredients, grilled to perfection. Elevate your dining experience with our mouthwatering Kababs, a delightful way to start your meal.. Price: Rs 100 per plate",
+      },
+      {
+        id: 16,
+        image: "../images/pepperchicken.jpeg",
+        name: "Pepper Chicken (Stater)",
+        category: "lunch",
+        price: "110₹",
+        description:
+          "Add a hint of zing to your start with our Pepper-inspired starters. Bursting with flavor, these appetizers bring a touch of spice to awaken your palate. Elevate your dining experience with our tempting Pepper starters, a delightful way to ignite your taste buds. Price: Rs 110 per plate",
+     },
+    {
+      id: 18,
+      image: "../images/parota.jpg",
+      name: "Parota",
+      category: "dinner",
+      price: "40₹",
+      description:
+        "Savor the flaky deliciousness of our Parota, a soft and layered bread that's a perfect match for any curry. Made with care and served fresh for your enjoyment. Elevate your dining experience with this classic Indian bread, a delightful addition to your meal.. Price Rs 40 per plate ",
+    },
+    {
+      id: 19,
+      image: "../images/bchicken.jpeg",
+      name: "Butter Chicken",
+      category: "dinner",
+      price: "140₹",
+      description:
+        "Indulge in the creamy delight of our Butter Chicken. Tender chicken cooked in a rich, flavorful tomato-based sauce. Elevate your dining experience with this beloved dish, a harmony of taste and comfort in every bite. Price Rs 140 per plate ",
+    },
+    {
+      id: 20,
+      image: "../images/chapati.jpeg",
+      name: "Chapati",
+      category: "dinner",
+      price: "40₹",
+      description:
+        "Enjoy the simplicity of our Chappathi, a soft and versatile Indian bread. Made from whole wheat dough, it's a perfect companion to curries or as a wrap. Elevate your dining experience with this classic staple, a light and satisfying addition to your meal. Price Rs 40 per plate",
+    },
+    ];
+  
+  
+  export default Menu;
+  
